3623b8624b9a0a56c4a22b7d756277b16005ef77
Volodymyr <volodyaruzhak@gmail.com>
2025-01-14 21:25:41 -0500

    Improve error boundry add tests

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,16 @@ import { MobileEditor, MobilePreview } from "./components";
 
 function App() {
 
+  // const ProblemComponent = () => {
+  //   throw new Error("Test error from ProblemComponent!");
+  // };
+
+
   return (
     <div className="flex h-screen">
       <MobileEditor />
       <MobilePreview />
+      {/* <ProblemComponent /> */}
     </div>
   );
 }
--- src/components/ErrorBoundary/ErrorBoundry.tsx
+++ src/components/ErrorBoundary/ErrorBoundry.tsx
@@ -27,19 +27,46 @@ export class ErrorBoundary extends React.Component<
         console.error("Uncaught Error:", error, info);
     }
 
+    handleReload = () => {
+        this.setState({ hasError: false });
+        window.location.reload();
+    };
+
+    handleDemoAlert = () => {
+        alert("This is just a demo. Support is not available.");
+    };
+
     render() {
         if (this.state.hasError) {
             return (
                 this.props.fallback || (
-                    <div className="p-4 text-center">
-                        <h2 className="text-2xl font-bold mb-2">Something went wrong.</h2>
-                        <p className="text-gray-600">
-                            We&apos;re sorry, but an unexpected error has occurred.
+                    <div className="flex flex-col items-center p-6 text-center">
+                        <h2 className="text-2xl font-bold mb-4">
+                            Oops! Something went wrong.
+                        </h2>
+                        <p className="text-gray-600 mb-4">
+                            We're sorry for the inconvenience. You can try
+                            reloading the page or contact support for further
+                            assistance.
                         </p>
+                        <div className="w-full md:w-1/2 xl:w-1/4 flex items-center justify-around">
+                            <button
+                                onClick={this.handleReload}
+                                className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+                            >
+                                Reload Page
+                            </button>
+                            <button
+                                onClick={this.handleDemoAlert}
+                                className="px-4 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-600"
+                            >
+                                Contact Support (Demo)
+                            </button>
+                        </div>
                     </div>
                 )
             );
         }
         return this.props.children;
     }
-}
+};
