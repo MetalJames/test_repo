89829ceab1ba22e68a7ea0c4a53acb8c70d24773
Volodymyr <volodyaruzhak@gmail.com>
2025-01-16 13:10:10 -0500

    fix typo in index.ts

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,7 +1,7 @@
 export { AddEditImageModal } from './AddEditImageModal';
 export { ColorPicker } from './ColorPicker';
 export { ConfirmationModal } from './ConfirmationModal';
-export { FeedbackModal } from './FeedBackModal';
+export { FeedbackModal } from './FeedbackModal';
 export { InputWithLabel } from './InputWithLabel';
 export { SaveConfigModal } from './SaveConfigModal';
 export { SelectWithLabel } from './SelectWithLabel';
